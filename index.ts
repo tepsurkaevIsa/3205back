1fb20529a4fd59895927c74b505f85a87480e93d
tepsurkaevIsa <tepsurkaev99999@mail.ru>
2023-11-28 00:30:52 +0300

    3205

--- index.ts
+++ index.ts
@@ -7,22 +7,9 @@ import fs from 'fs';
 
 const app: Express = express();
 
-app.use(cors({
-  origin: 'https://3205front-z59a.vercel.app',  // Укажите адрес вашего фронтенда
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 
 interface User {
