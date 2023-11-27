b7dd7bbde9b4928e239b5964f6d0f1a607203d43
tepsurkaevIsa <tepsurkaev99999@mail.ru>
2023-11-27 22:34:48 +0300

    back

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+const usersFilePath = path_1.default.join(__dirname, 'index.json');
+const usersJson = fs_1.default.readFileSync(usersFilePath, 'utf-8');
+const usersObj = JSON.parse(usersJson);
+app.post('/', (req, res) => {
+    const { email, number } = req.body;
+    const users = usersObj.filter((item) => {
+        if (number) {
+            return item.email === email && item.number === number;
+        }
+        else {
+            return item.email === email;
+        }
+    });
+    setTimeout(() => {
+        users && res.json(users);
+        console.log(users);
+    }, 5000);
+});
+const PORT = 3040;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ index.ts
@@ -0,0 +1,42 @@
+
+
+import express, { Request, Response, Express } from 'express';
+import cors from 'cors';
+import path from 'path';
+import fs from 'fs';
+
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+
+interface User {
+  email: string;
+  number: string;
+}
+
+const usersFilePath: string = path.join(__dirname, 'index.json');
+const usersJson: string = fs.readFileSync(usersFilePath, 'utf-8');
+const usersObj: User[] = JSON.parse(usersJson);
+
+app.post('/', (req: Request, res: Response) => {
+  const { email, number }: { email: string; number?: string } = req.body;
+  const users: User[] = usersObj.filter((item: User) => {
+    if (number) {
+      return item.email === email && item.number === number;
+    } else {
+      return item.email === email;
+    }
+  });
+
+  setTimeout(() => {
+    users && res.json(users);
+    console.log(users);
+  }, 5000); 
+
+});
+
+const PORT: number = 3040;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
